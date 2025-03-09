116a4559396e150878f86ad9235ea291802f7064
Rawan Ghobar <rawanghobar@gmail.com>
2025-03-09 02:53:42 +0200

    Creating Questions table

---
+++ Article-server/Database/Migrations/create_questions_table.php
@@ -0,0 +1,17 @@
+<?php
+include(__DIR__ . "/../../Connection/connection.php");
+
+$sql= "CREATE TABLE questions(
+                              id int AUTO_INCREMENT PRIMARY KEY,
+                              question varchar(255) NOT NULL,
+                              answer text NOT NULL
+                              )";
+
+if ($mysqli->query($sql) === TRUE)
+{
+    echo "Questions table created successfully";
+    return;
+}
+
+    echo "Cannot create table questions" .$mysqli->error;
+?>
