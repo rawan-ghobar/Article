90d9e11942c7b1208bde4586abaa11ca2d0b530a
Rawan Ghobar <rawanghobar@gmail.com>
2025-03-09 19:41:11 +0200

    Creating the QuestSkeleton

---
+++ Article-server/Models/QuestSkeleton.php
@@ -0,0 +1,31 @@
+<?php
+
+class QuestionSkeleton
+{
+    private $question;
+    private $answer;
+
+    public function __construct($question,$answer)
+    {
+        $this->question= $question;
+        $this->answer= $answer;
+    }
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    public function getAnswer() {
+        return $this->answer;
+    }
+
+    public function setQuestion($question)
+    {
+        $this->question= $question;
+    }
+
+    public function setAnswer($answer)
+    {
+        $this->answer= $answer;
+    }
+}
